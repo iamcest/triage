a4e8af7b30d56e75e3a5be523cd050663110a85e
iamcest <44790773+iamcest@users.noreply.github.com>
2020-06-06 16:13:45 -0300

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,8 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
+mongoose.connect("mongodb://localhost:27017/iamcest-triage");
+ 
+// mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
 
 module.exports = {mongoose};
