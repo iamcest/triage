5c025fc41ef142b94301543516eec87001606f09
iamcest <44790773+iamcest@users.noreply.github.com>
2020-06-06 20:51:05 -0300

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds133814.mlab.com:33814/heroku_r379skr8
+mongoose.connect("mongodb://heroku_r379skr8:Mongodb2020@ds133814.mlab.com:33814/heroku_r379skr8
 // mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
 
 module.exports = {mongoose};
