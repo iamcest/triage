0bd74473644030723c7354ffbd01e3bed13b4890
iamcest <44790773+iamcest@users.noreply.github.com>
2020-06-06 22:20:27 -0300

    Update addpatient.js

--- public/scripts/addpatient.js
+++ public/scripts/addpatient.js
@@ -21,13 +21,13 @@ $(document).ready(function() {
                columns:
                [
                    {
-                       title: "Disease"
+                       title: "Enfermedad"
                    },
                    {
-                       title: "Score"
+                       title: "Riesgo"
                    },
                    {
-                       title: "Diagnosis"
+                       title: "Diagnóstico"
                    },
                ],
                scrollY: '40vh',
@@ -35,19 +35,8 @@ $(document).ready(function() {
                paging: false,
                info: false,
                language: {
-                 sSearch: "Search disease"
+                 sSearch: "Buscar x Enfermedad"
                }
           });
      });
 });
-
-/*
-     Google analytics
-*/
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-97568701-1', 'auto');
-ga('send', 'pageview');
