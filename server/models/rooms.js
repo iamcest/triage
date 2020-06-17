6fd631fd03e083be4e7d56ae6ac4bd8c8b6c865c
iamcest <44790773+iamcest@users.noreply.github.com>
2020-06-16 22:49:09 -0300

    Update rooms.js

--- server/models/rooms.js
+++ server/models/rooms.js
@@ -26,6 +26,11 @@ var rooms = {};
 rooms["noroom"] = false;
 
 
+validate: { 
+  validator: value => validator.isURL(value, { protocols: ['http','https','ftp'], require_tld: true, require_protocol: true }),
+  message: 'Must be a Valid URL' 
+}
+
 /*
 	Function to put the default diseases in the system
 */
